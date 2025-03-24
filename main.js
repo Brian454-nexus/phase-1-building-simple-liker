ee8ba32a8d1d74d13b3867d78b939e5ba7217885
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-03-24 14:50:59 +0300

    Default

--- main.js
+++ main.js
@@ -36,3 +36,19 @@ else if (heart.innerHTML === FULL_HEART) {
 }
 });
 });
+
+//------------------------------------------------------------------------------
+// Don't change the code below: this function mocks the server response
+//------------------------------------------------------------------------------
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let isRandomFailure = Math.random() < .2;
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
