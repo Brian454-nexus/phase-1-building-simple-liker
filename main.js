d9e55a6246530813f602b6ccfad34f36462f34bd
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-03-24 14:43:25 +0300

    Added modal and message elements

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡';
 const FULL_HEART = '♥';
+
+// Get the modal and message elements from the DOM
+const modal = document.getElementById("modal");
+const modalMessage = document.getElementById("modal-message");
